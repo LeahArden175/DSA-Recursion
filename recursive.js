8361b57725577494419fb8865dd75e51271a3b22
Leah Arden <leaharden175@gmail.com>
2020-11-30 12:10:09 -0500

    1-6 done

--- recursive.js
+++ recursive.js
@@ -1,14 +1,78 @@
 //1. Counting Sheep! Write a function that counts sheep jumping over fence
-const countSheep = function(totalSheep) {
-    if(totalSheep < 0) {
-      console.log('there are no sheep to count')
-    } else if (totalSheep > 0) {
-        console.log(`${totalSheep}: Another sheep jumps over the fence`);
-        countSheep(totalSheep - 1)
+const countSheep = function (totalSheep) {
+  if (totalSheep < 0) {
+    return "there are no sheep to count";
+  } else if (totalSheep > 0) {
+    return (
+      `${totalSheep}: Another sheep jumps over the fence \n` +
+      countSheep(totalSheep - 1)
+    );
+  } else {
+    return `All sheep jumped over the fence`;
+  }
+};
+
+console.log(countSheep(4));
+
+//2. Write a function called powerCalculator() that takes two parameters, an integer as a base, and another integer as an exponent.
+
+const powerCalculator = function (base, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+  if (exp < 0) {
+    return "Exponent should be >= 0";
+  } else {
+    return base * powerCalculator(base, exp - 1);
+  }
+};
+
+console.log(powerCalculator(10, 2));
+
+//3. Write a function that reverses a string.
+//Take a string as input, reverse the string, and return the new string.
+
+const reverseString = function (string) {
+  if (!string) {
+    return "";
+  }
+  return reverseString(string.slice(1)) + string[0];
+};
+
+console.log(reverseString("hello"));
+
+//4. Calculate the nth triangular number
+
+
+//TODO!!!!!!!!!!
+
+//5. String Splitter
+
+const splitter = function(string, separator) {
+    if(!string) {
+      return ''
+    }
+    let number = string[0]
+  
+    if(number === separator) {
+      number = ''
+    }
+    return number + splitter(string.slice(1), separator)
+  }
+  
+  console.log(splitter('02/20/2020', '/')) //NOT CORRECT BUT CLOSE
+
+  //6. Fibonacci
+  const fibonaci = function(num) {
+    if(num <= 0) {
+      return 0
+    } else if(num === 1){
+      return 1
     } else {
-      console.log(`All sheep jumped over the fence`)
+      return fibonaci(num - 1) + fibonaci(num - 2)
     }
   }
   
-  let allSheep = 3
-  console.log(countSheep(allSheep))
+  console.log(fibonaci(4))
+
+  //7. Factorial!  Write a recursive function that finds the factorial of a given number.
